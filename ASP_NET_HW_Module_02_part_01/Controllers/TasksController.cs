9d519be8e1c9ef2a6a58cf14e101588b7f5871a2
Kirill Lagutin <lagutin.k@mail.ru>
2022-08-04 02:43:48 +0300

    Добавьте файлы проекта.

---
+++ ASP_NET_HW_Module_02_part_01/Controllers/TasksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_HW_Module_02_part_01.Controllers
+{
+    public class TasksController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Task1()
+		{
+			return View();
+		}
+
+        public ActionResult Task2()
+        {
+            return View();
+        }
+	}
+}
---
+++ ASP_NET_HW_Module_02_part_01/Models/Enimal.cs
@@ -0,0 +1,7 @@
+﻿namespace ASP_NET_HW_Module_02_part_01.Models
+{
+	public class Enimal
+	{
+
+	}
+}
